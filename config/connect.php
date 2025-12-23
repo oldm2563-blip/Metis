50b24936f77685691b59eaa811c09ae84f462238
oldman <oldm2563@email.com>
2025-12-23 22:35:24 +0100

    returning a value

--- config/connect.php
+++ config/connect.php
@@ -10,6 +10,7 @@
         try{
              $connect = new PDO("mysql:host=" . $this->server . ";port=" . $this->port . ";dbname=" . $this->db_name, $this->user, $this->pass);
             $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $connect;
         }
          catch(PDOException $e){
         echo "Connection Failed :" . $e->getMessage(); 
