a33ed9f32b2bd650e71d45a0da77d393dc5a7fc2
oldman <oldm2563@email.com>
2025-12-24 14:52:12 +0100

    add the adding method for short projects

--- src/classes/Shortpr.php
+++ src/classes/Shortpr.php
@@ -1,5 +1,12 @@
 <?php
     class Shortpr extends Project{
 
+        private $project_type = "ShortProject";
+
+        public function addproject(){
+            $query = "INSERT INTO projects (project_name, project_type, member_id) VALUES (? , ? , ?)";
+            $stmt = $this->connect()->prepare($query);
+            $stmt->execute([$this->project_name, $this->project_type, $this->member_id]);
+        }
     }
 ?>
